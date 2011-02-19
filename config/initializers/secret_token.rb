63388859006f74178589dab0f3f2ebeaf7fce1b1
Ted Fitzgerald <ted.fitzgerald@gmail.com>
2011-02-19 17:46:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arvore::Application.config.secret_token = '38ff75359db376282224cca8b7a0c7352d452b1c93ee87ec893482b496090b406fea50b75c1a25bb1e4dc0c19c63b5de61abf46a9ffae429cb121981a6157a17'
