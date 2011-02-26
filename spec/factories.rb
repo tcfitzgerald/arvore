00dafbbee585c6a9ebcc26aa332b36e075ee5b0c
Ted Fitzgerald <ted.fitzgerald@gmail.com>
2011-02-26 13:55:36 -0600

    user model with passwords and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Ted Fitzgerald"
+  user.email                 "ted.fitzgerald@gmail.com"
+  user.password              "qwerty"
+  user.password_confirmation "qwerty"
+end
